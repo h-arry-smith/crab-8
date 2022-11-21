3a2d17889f17d5bc1efa018ce39b5f41ed964001
Harry Smith <harry@hsmith.net>
2022-11-21 21:17:14 +0300

    Basic CLI argument handling

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,19 @@
-// Reference: http://devernay.free.fr/hacks/chip8/C8TECH10.HTM
+use std::{env, process};
 
-// 2.1 - Memory
-// Most Chip-8 programs start at location 0x200 (512), but some begin at
-// 0x600 (1536). Programs beginning at 0x600 are intended for the ETI 660
-// computer.
-const NORMAL_START_INDEX: usize = 512;
-const ETI_660_START_INDEX: usize = 1526;
-
-struct Chip8 {
-    // 2.1 - Memory
-    // The Chip-8 language is capable of accessing up to 4KB (4,096 bytes) of
-    // RAM, from location 0x000 (0) to 0xFFF (4095).
-    ram: [u8; 4096],
-
-    // 2.2 - Registers
-    registers: Registers,
-    // There are also some "pseudo-registers" which are not accessable from
-    // Chip-8 programs.
-
-    // The program counter (PC) should be 16-bit, and is used to store the
-    // currently executing address.
-    pc: u16,
-
-    // The stack pointer (SP) can be 8-bit, it is used to point to the topmost
-    // level of the stack.
-    sp: u8,
-
-    // The stack is an array of 16 16-bit values, used to store the address that
-    // the interpreter shoud return to when finished with a subroutine. Chip-8
-    // allows for up to 16 levels of nested subroutines.
-    stack: [u16; 16],
-}
-
-// 2.2 - Registers
-struct Registers {
-    // Chip-8 has 16 general purpose 8-bit registers, usually referred to as Vx,
-    // where x is a hexadecimal digit (0 through F).
-    v_0: u8,
-    v_1: u8,
-    v_2: u8,
-    v_3: u8,
-    v_4: u8,
-    v_5: u8,
-    v_6: u8,
-    v_7: u8,
-    v_8: u8,
-    v_9: u8,
-    v_a: u8,
-    v_b: u8,
-    v_c: u8,
-    v_d: u8,
-    v_e: u8,
-    v_f: u8,
-
-    // There is also a 16-bit register called I. This register is generally
-    // used to store memory addresses
-    i: u16,
-}
+use flake_8::chip8::Chip8;
 
 fn main() {
-    println!("Hello, world!");
+    let mut cpu = Chip8::new();
+
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Usage: flake-8 [path]");
+        process::exit(1);
+    }
+
+    let path = args.get(1).expect("A file path must be provided.");
+
+    cpu.load_rom(path);
+    cpu.run();
 }
