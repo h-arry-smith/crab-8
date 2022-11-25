19000960e0b3bb6c878fd389705c346fc70dc0aa
Harry Smith <harry@hsmith.net>
2022-11-25 07:11:56 +0300

    Faulty assumption on overflowing_sub causes incorrect behaviour for SUB instruction

--- src/chip8.rs
+++ src/chip8.rs
@@ -359,15 +359,14 @@ impl Chip8 {
         let vy = self.registers.get(y);
 
         // If Vx > Vy, then VF is set to 1, otherwise 0.
-        // Then Vy is subtracted from Vx, and the results stored in Vx.
-        let (result, borrow) = vx.overflowing_sub(vy);
-
-        if borrow {
-            self.registers.v_f = 1;
+        if vx > vy {
+            self.registers.v_f = 1
         } else {
-            self.registers.v_f = 0;
+            self.registers.v_f = 0
         }
 
+        // Then Vy is subtracted from Vx, and the results stored in Vx.
+        let (result, _) = vx.overflowing_sub(vy);
         self.registers.put(x, result);
 
         self.pc + 2
